fe9bbb123d6b778cd4e178f920b18cdf0cbeabdd
Michal@SFB2 <michal.marusan@gmail.com>
2021-01-09 23:20:13 +0100

    sastoken-update

--- ImageResizeWebApp/ImageResizeWebApp/Helpers/StorageHelper.cs
+++ ImageResizeWebApp/ImageResizeWebApp/Helpers/StorageHelper.cs
@@ -1,6 +1,7 @@
 ﻿using Azure.Storage;
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
+using Azure.Storage.Sas;
 using ImageResizeWebApp.Models;
 using Microsoft.AspNetCore.Http;
 using System;
